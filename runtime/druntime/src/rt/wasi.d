15e377e35e9dd3a3bad3ee629a1fd0ecc3516d28
cbleser <cr@tagion.org>
2024-03-06 12:54:14 +0200

    WIP: runtime/druntime/src/rt/wasi.d dummy _d_eh_swapContextDwarf

--- runtime/druntime/src/rt/wasi.d
+++ runtime/druntime/src/rt/wasi.d
@@ -6,3 +6,11 @@ extern(C) void _d_throw_exception(Throwable o)
     mixin WASIError;
     assert(0, wasi_error);
 }
+
+extern(C) void* _d_eh_swapContextDwarf(void* newContext) nothrow @nogc
+{
+    import core.sys.wasi.missing;
+    mixin WASIError;
+    assert(0, wasi_error);
+}
+
