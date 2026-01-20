22ed030fc24981909175ae8bab1e10d759340e36
cbleser <cr@tagion.org>
2026-01-20 17:17:24 +0200

    wip(wasi): core.thread.threadbase restored from master

--- runtime/druntime/src/core/thread/threadbase.d
+++ runtime/druntime/src/core/thread/threadbase.d
@@ -307,11 +307,6 @@ class ThreadBase
      */
     @property bool isRunning() nothrow @nogc
     {
-        version(WASI)
-        {
-            /// Threads not suppoted yet
-            return true;
-        }
         if (m_addr == m_addr.init)
             return false;
 
